8ffdb29b70e409e0e7422feb213e09d5c578d507
Barry Williams <barry.williams@linn.co.uk>
2018-08-07 14:36:05 +0100

    Implemented power proxy

--- src/proxies/LinnApiDevicesProxy.ts
+++ src/proxies/LinnApiDevicesProxy.ts
@@ -19,19 +19,28 @@ interface LinkResource {
 class LinnApiDevicesProxy implements ILinnApiDevicesProxy {
     constructor(private apiRoot : string) {
     }
-    async list(token : string): Promise<IEndpoint[]> {
-        let options = {
+
+    private headers(token : string) {
+        return {
             headers: {
                 'Accept': 'application/json',
                 'Authorization': `Bearer ${token}`
             }
         };
+    }
 
-        let devices = await webRequest.json<AssociatedDevice[]>(`${this.apiRoot}/devices/`, options);
+    async list(token : string): Promise<IEndpoint[]> {
+        let devices = await webRequest.json<AssociatedDevice[]>(`${this.apiRoot}/devices/`, this.headers(token));
         
         return devices.map(d => new SpeakerEndpoint(d.id, d.name, d.model));
     }
+
     async setStandby(deviceId : string, value : boolean, token : string): Promise<void> {
+        if (value) {
+            await webRequest.put(`${this.apiRoot}/devices/${deviceId}/standby`, this.headers(token));
+        } else {
+            await webRequest.delete(`${this.apiRoot}/devices/${deviceId}/standby`, this.headers(token));
+        }
     }
 }
 
